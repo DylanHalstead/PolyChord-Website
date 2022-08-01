b3743dd0bc3f7ca02efc66d71d71998251b0f34c
DylanHalstead <dylanhalstead11@gmail.com>
2022-08-01 11:32:56 +0100

    adding node_modules for test purposes, further work on about us, changed default font-size

---
+++ js/carousel.js
@@ -0,0 +1,14 @@
+new Glide('.glide', {
+    type: 'carousel',
+    perView: 3,
+    gap: 30,
+    animationDuration: 750,
+    breakpoints: {
+        1024: {
+            perView: 2
+        },
+        768: {
+            perView: 1
+        }
+    }
+}).mount();
--- js/hamburger.js
+++ js/hamburger.js
@@ -12,19 +12,3 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
-
-// Carousel Code
-new Glide('.glide', {
-    type: 'carousel',
-    perView: 3,
-    gap: 30,
-    animationDuration: 750,
-    breakpoints: {
-        1024: {
-            perView: 2
-        },
-        768: {
-            perView: 1
-        }
-    }
-}).mount();
