7da2e0676f2900f9fef56e5c28027ca68588bbd4
DylanHalstead <dylanhalstead11@gmail.com>
2022-08-15 13:01:42 -0400

    general flex boxes added to pages

--- js/sketch.js
+++ js/sketch.js
@@ -5,7 +5,7 @@ let sketch1 = new p5(( sketch ) => {
     let particle3;
 
     sketch.setup = () => {
-        canvas = sketch.createCanvas(300, 300);
+        canvas = sketch.createCanvas(400, 400);
         const width = sketch.width
         const height = sketch.height
         // Walls around perimeter
@@ -62,7 +62,7 @@ let sketch1 = new p5(( sketch ) => {
         particle3.show();
         particle3.look();
     }
-}, document.querySelector('#canvas-1'));
+}, document.querySelector('#midas-canvas-1'));
 
 let sketch2 = new p5(( sketch ) => {
     let walls = [];
@@ -128,7 +128,7 @@ let sketch2 = new p5(( sketch ) => {
         particle3.show();
         particle3.look();
     }
-}, document.querySelector('#canvas-2'));
+}, document.querySelector('#midas-canvas-2'));
 
 let sketch3 = new p5(( sketch ) => {
     let walls = [];
@@ -259,7 +259,7 @@ let sketch3 = new p5(( sketch ) => {
             }
         }, 2000)
     }
-}, document.querySelector('#canvas-3'));
+}, document.querySelector('#midas-canvas-3'));
 
 
 
@@ -365,74 +365,7 @@ let sketch4 = new p5(( sketch ) => {
             particle3.fade(0, .02);
         }
     }
-}, document.querySelector('#canvas-4'));
-
-let sketch5 = new p5(( sketch ) => {
-    let walls = [];
-    let particle1;
-    let particle2;
-    let particle3;
-    let particle4;
-    let particle5;
-    
-    sketch.setup = () => {
-        canvas = sketch.createCanvas(300, 300);
-        const width = sketch.width
-        const height = sketch.height
-        // Walls around perimeter
-        walls.push(new Boundary(sketch, 0, 0, width, 0));
-        walls.push(new Boundary(sketch, width, 0, width, height));
-        walls.push(new Boundary(sketch, width, height, 0, height));
-        walls.push(new Boundary(sketch, 0, height, 0, 0));
-    
-        // Building demo, all in terms a width/height to be scalable
-        walls.push(new Boundary(sketch, 0, height/7, width, height/7));
-    
-        walls.push(new Boundary(sketch, 0, 2 * height/7, width/7, 2 * height/7));
-        walls.push(new Boundary(sketch, width/7, 2 * height/7, width/7, height));
-    
-        walls.push(new Boundary(sketch, 6 * width/7, 3 * height/7, width, 3 * height/7));
-        walls.push(new Boundary(sketch, 6 * width/7, 3 * height/7, 6 * width/7, height));
-    
-        walls.push(new Boundary(sketch, width/4 - width/80, 6 * height/7, width/4 - width/80, height));
-        walls.push(new Boundary(sketch, width/4 - width/80, 6 * height/7, 3 * width/4 + width/80, 6 * height/7));
-        walls.push(new Boundary(sketch, 3 * width/4 + width/80, 6 * height/7, 3 * width/4 + width/80, height));
-    
-        walls.push(new Boundary(sketch, width/4 - width/80, 4 * height/7 + height/20, width/4 - width/80, 5 * height/7 + height/20));
-        walls.push(new Boundary(sketch, width/4 - width/80, 5 * height/7 + height/20, 11 * width/20, 5 * height/7 + height/20));
-        walls.push(new Boundary(sketch, 11 * width/20, 4 * height/7 + height/20, 11 * width/20, 5 * height/7 + height/20));
-        walls.push(new Boundary(sketch, width/4 - width/80, 4 * height/7 + height/20, 11 * width/20, 4 * height/7 + height/20));
-    
-        walls.push(new Boundary(sketch, 3 * width/4 + width/80, height/2, 3 * width/4 + width/80, 5 * height/7 + height/20));
-        walls.push(new Boundary(sketch, 5 * width/8 - width/60, height/2, 5 * width/8 - width/60, 5 * height/7 + height/20));
-        walls.push(new Boundary(sketch, 3 * width/4 + width/80, height/2, 5 * width/8 - width/60, height/2));
-        walls.push(new Boundary(sketch, 3 * width/4 + width/80, 5 * height/7 + height/20, 5 * width/8 - width/60, 5 * height/7 + height/20));
-    
-        walls.push(new Boundary(sketch, 4 * width/10, 2 * height/7, width/2, 2 * height/7));
-        walls.push(new Boundary(sketch, 4 * width/10, 2 * height/7, 4 * width/10, height/10 + 2 * height/7));
-        walls.push(new Boundary(sketch, width/2, 2 * height/7, width/2, height/10 + 2 * height/7));
-        walls.push(new Boundary(sketch, 4 * width/10, height/10 + 2 * height/7, width/2, height/10 + 2 * height/7));
-    
-        // Setting up particles
-        particle1 = new Particle(sketch, width/2, height/2, walls, [243, 91, 4, 51]);
-        // particle1.setFOV(200, 100);
-        particle2 = new Particle(sketch, width/2, height/2, walls, [92, 173, 72, 51]);
-        particle3 = new Particle(sketch, width/2, height/2, walls, [183, 68, 184, 51]);
-        particle4 = new Particle(sketch, width/2, height/2, walls, [4, 139, 168, 51]);
-        particle5 = new Particle(sketch, width/2, height/2, walls, [210, 181, 55, 51]);
-    }
-
-    sketch.draw = () => {
-        sketch.background(255);
-        for (let i = 0; i < walls.length; i++) {
-            walls[i].show();
-        }
-    
-        particle1.updatePos(60, 151);
-        particle1.show();
-        particle1.look();
-    }
-}, document.querySelector('#canvas-5'));
+}, document.querySelector('#midas-canvas-4'));
 
 // Function used to check if an element is on screen
 function checkVisible(elm) {
